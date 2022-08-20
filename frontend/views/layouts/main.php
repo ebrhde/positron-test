6c18c997a962efef9fb1e0c6104f6afe6b1fa9ec
ebrhde <tharuthlessman@gmail.com>
2022-08-20 14:54:57 +0300

    Frontend layout update

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -26,39 +26,30 @@ AppAsset::register($this);
 <?php $this->beginBody() ?>
 
 <header>
-    <?php
-    NavBar::begin([
-        'brandLabel' => Yii::$app->name,
-        'brandUrl' => Yii::$app->homeUrl,
-        'options' => [
-            'class' => 'navbar navbar-expand-md navbar-dark bg-dark fixed-top',
-        ],
-    ]);
-    $menuItems = [
-        ['label' => 'Home', 'url' => ['/site/index']],
-        ['label' => 'About', 'url' => ['/site/about']],
-        ['label' => 'Contact', 'url' => ['/site/contact']],
-    ];
-    if (Yii::$app->user->isGuest) {
-        $menuItems[] = ['label' => 'Signup', 'url' => ['/site/signup']];
-    }
+    <nav class="navbar navbar-dark bg-dark navbar-expand-md position-relative" style="border-radius: 0">
+        <?= Html::a('Домашняя', '/', [
+                'class' => 'navbar-brand fs-2', 'style' => 'padding-top: 1.25rem;'
+        ]); ?>
 
-    echo Nav::widget([
-        'options' => ['class' => 'navbar-nav me-auto mb-2 mb-md-0'],
-        'items' => $menuItems,
-    ]);
-    if (Yii::$app->user->isGuest) {
-        echo Html::tag('div',Html::a('Login',['/site/login'],['class' => ['btn btn-link login text-decoration-none']]),['class' => ['d-flex']]);
-    } else {
-        echo Html::beginForm(['/site/logout'], 'post', ['class' => 'd-flex'])
-            . Html::submitButton(
-                'Logout (' . Yii::$app->user->identity->username . ')',
-                ['class' => 'btn btn-link logout text-decoration-none']
-            )
-            . Html::endForm();
-    }
-    NavBar::end();
-    ?>
+        <div class="position-absolute" style="right: 20px;">
+            <?php if (Yii::$app->user->isGuest): ?>
+                <?= Html::a('Вход', '/site/login', [
+                    'class' => 'btn btn-success mx-5',
+                ]); ?>
+                <?= Html::a('Регистрация', '/site/signup', [
+                    'class' => 'btn btn-warning'
+                ]); ?>
+            <?php else: ?>
+                <?= Html::beginForm(['/site/logout'], 'post', ['class' => 'd-flex'])
+                    . Html::submitButton(
+                    'Выход (' . Yii::$app->user->identity->username . ')',
+                        ['class' => 'btn btn-warning']
+                    )
+                    . Html::endForm();
+                ?>
+            <?php endif; ?>
+        </div>
+    </nav>
 </header>
 
 <main role="main" class="flex-shrink-0">
@@ -77,6 +68,46 @@ AppAsset::register($this);
         <p class="float-end"><?= Yii::powered() ?></p>
     </div>
 </footer>
+
+<div class="modal fade" id="signinModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                ...
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary">Save changes</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="modal fade" id="signupModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                ...
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary">Save changes</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+<style>
+    html { font-size: 12px; }
+</style>
 
 <?php $this->endBody() ?>
 </body>
