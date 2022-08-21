335deabb1d2d0172c3ac6bc8b108bc7d7271f8c5
ebrhde <tharuthlessman@gmail.com>
2022-08-22 01:51:11 +0300

    get photo method

--- common/models/Book.php
+++ common/models/Book.php
@@ -2,10 +2,13 @@
 
 namespace common\models;
 
+use Imagine\Image\Box;
+use Imagine\Image\ManipulatorInterface;
 use Yii;
 use yii\db\Expression;
 use yii\helpers\ArrayHelper;
 use yii\helpers\Html;
+use yii\imagine\Image;
 
 /**
  * This is the model class for table "book".
@@ -30,7 +33,11 @@ class Book extends \yii\db\ActiveRecord
     public $file = null;
     public $removeImage = null;
     public $categoriesBook = null;
-    private $_assetsPath = '@backend/web/uploads/books';
+
+    private $_assetsUrl = '/uploads/books/cache';
+    private $_assetsOrigPath = '@backend/web/uploads/books';
+    private $_assetsPath = '@webroot/uploads/books/cache';
+
 
     const STATUS_ACTIVE = 1;
     const STATUS_DELETED = 9;
@@ -165,5 +172,73 @@ class Book extends \yii\db\ActiveRecord
     public static function getOptions()
     {
         return self::$_options;
+    }
+
+    public function getPhoto($width = 0, $height = 0, $method = 'thumb')
+    {
+        $origDir = Yii::getAlias($this->_assetsOrigPath);
+        $pathDir = Yii::getAlias($this->_assetsPath);
+
+        if (!$this->thumbnail || !is_file($origDir . '/' . $this->thumbnail))
+            $this->thumbnail = 'book-default.jpg';
+
+        $pathImage = $origDir . '/' . $this->thumbnail;
+
+        $salt = '';
+        if (is_file($pathImage))
+            $salt = sha1_file($pathImage);
+
+        if (!is_dir($pathDir))
+            mkdir($pathDir, 0775, true);
+
+        if ($width || $height) {
+            if ($method == 'resize') {
+                $imageCache = $pathDir . '/r-' . $salt . '-new-' . $width . 'x' . $height . '-' . $this->id . '.jpg';
+
+                if (!is_file($imageCache)) {
+
+                    $box = new Box($width, $height);
+
+                    if (is_file($pathImage))
+                        Image::getImagine()->open($pathImage)->resize($box)->save($imageCache, ['quality' => 90]);
+                }
+
+                if (is_file($imageCache))
+                    return $this->_assetsUrl . '/r-' . $salt . '-new-' . $width . 'x' . $height . '-' . $this->id . '.jpg';
+
+            } elseif ($method == 'thumb') {
+                $imageCache = $pathDir . '/t-' . $salt . '-new-' . $width . 'x' . $height . '-' . $this->id . '.jpg';
+
+                if (!is_file($imageCache)) {
+                    $newW = $width;
+                    $newH = $height;
+
+                    if (is_file($pathImage)) {
+                        $temp = Image::getImagine()->load(file_get_contents($pathImage));
+                        $size = $temp->getSize();
+
+                        if (($size->getWidth() / $size->getHeight()) > ($width / $height))
+                            $newW = $newH * ($size->getWidth() / $size->getHeight());
+                        else
+                            $newH = ($size->getHeight() / $size->getWidth()) * $newW;
+
+                        Image::resize($pathImage, $newW, $newH, true, true)
+                            ->thumbnail(new Box($width, $height), ManipulatorInterface::THUMBNAIL_OUTBOUND)
+                            ->save($imageCache, ['jpeg_quality' => 90]);
+                    }
+                }
+
+                if (is_file($imageCache))
+                    return $this->_assetsUrl . '/t-' . $salt . '-new-' . $width . 'x' . $height . '-' . $this->id . '.jpg';
+            }
+        } else {
+            $imageCache = $pathDir . '/o-' . $salt . '-new-' . $this->id . '.jpg';
+            Image::getImagine()->open($pathImage)->save($imageCache, ['quality' => 90]);
+
+            if (is_file($imageCache))
+                return $this->_assetsUrl . '/o-' . $salt . '-new-' . $this->id . '.jpg';
+        }
+
+        return null;
     }
 }
